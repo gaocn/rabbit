10e82091e16725477e561ff81936c7147f693677
gaocn <gaowenwencn@163.com>
2022-02-02 10:47:11 +0800

    轮播组件实现

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,28 @@
 <template>
+<div class="home-entry">
   <div class="container">
+    <!-- 左侧分类 -->
     <home-category />
+
+    <!-- 轮播图 -->
+    <home-banner />
   </div>
+</div>
 </template>
 
 <script>
 import HomeCategory from './home-category.vue'
+import HomeBanner from './home-banner.vue'
 export default {
   name: 'Home',
-  components: { HomeCategory }
+  components: { HomeCategory, HomeBanner }
 
 }
 </script>
 
 <style lang="less" scoped>
 .home-container {
-  .hoverShadow();
-  color: @warnColor;
+  // .hoverShadow();
+  // color: @warnColor;
 }
 </style>
